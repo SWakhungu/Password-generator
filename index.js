b915f6ef5510c6036e791309b8d7861bccde0ceb
Steve Wakhungu <stwakhungu@gmail.com>
2022-08-17 18:33:11 +0300

    Correct code for rendering both generated passwords

--- index.js
+++ index.js
@@ -1,26 +1,30 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
-let btnEl = document.getElementById("btnel")
+let btnEl = document.getElementById("btnel");
+let btnEl2 = document.getElementById("btnel2");
 // console.log(btnEl)
 let passwordLength = 12
 
 function getRandomCharacter(){
-    let randomChar = Math.floor(Math.random() * characters.length)
+    let randomChar = Math.floor(Math.random() * characters.length);
     return characters[randomChar]
 }
 
 function generateRandomPassword(){
-    let randomPassword = " "
+    let randomPassword = " ";
+    let randomPassword2 = " ";
     for (let i = 0; i < passwordLength; i++){
-        randomPassword += getRandomCharacter()
+        randomPassword += getRandomCharacter();
+        randomPassword2 += getRandomCharacter()
     }
-    btnEl.textContent = randomPassword
+    btnEl.textContent = randomPassword;
+    btnEl2.textContent = randomPassword2;
     return randomPassword
 }
 
-const generatedPasswordOne = generateRandomPassword()
+const generatedPasswordOne = generateRandomPassword();
 console.log("Here is a random password: ", generatedPasswordOne)
 
-const generatedPasswordTwo = generateRandomPassword()
+const generatedPasswordTwo = generateRandomPassword();
 console.log("Your random password is: ", generatedPasswordTwo)
